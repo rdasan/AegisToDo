6de00deb606b8ecee5507724f7d78a1e7f25c600
rdasan <rejidasan82@gmail.com>
2017-02-04 18:24:40 -0700

    Adding a ToDoItem Model for creating a Table

---
+++ AegisToDo.Entities/AegisToDo.Entities/Model/ToDoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AegisToDo.Entities.Model
+{
+    public class ToDoItem
+    {
+        [Key]
+        public int ItemId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+    }
+}
